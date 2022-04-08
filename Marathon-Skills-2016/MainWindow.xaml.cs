7ef24775167e48f9fce11bc4b96c23d22e194d83
1000-7 <0>
2022-04-08 15:53:31 +0300

    Добавьте файлы проекта.

---
+++ Marathon-Skills-2016/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Marathon_Skills_2016
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //// Code To transfer to hex from RGB
+            //   var color = Color.FromRgb(R, G, B);
+            //   string hex = color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+            InitializeComponent();
+        }
+    }
+}
