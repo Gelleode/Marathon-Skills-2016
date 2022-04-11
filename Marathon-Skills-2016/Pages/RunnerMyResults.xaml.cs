cca29ed55ffe28a84003dded051debd264897763
Gelleode <danilas444@gmail.com>
2022-04-11 23:40:57 +0300

    Added MyResults page

---
+++ Marathon-Skills-2016/Pages/RunnerMyResults.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Marathon_Skills_2016.DBModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Marathon_Skills_2016.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RunnerMyResults.xaml
+    /// </summary>
+    public partial class RunnerMyResults : Page
+    {
+        private static User _user;
+        public RunnerMyResults(User user)
+        {
+            _user = user;
+            InitializeComponent();
+            TBlockGender.Text = _user.Runner.First().Gender;
+        }
+    }
+}
